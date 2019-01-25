841ab044d170569ec9ca6fbd4c2653074b6297fd
arinmsn <arinmsn@gmail.com>
2019-01-24 16:23:15 -0800

    app working - improvements to follow

--- assets/javascript/source.js
+++ assets/javascript/source.js
@@ -1,112 +1,125 @@
-$(window).ready(function() {
-    var topics = [
-        "Home Alone",
-        "Alice in Wonderland",
-        "Lord of the Rings",
-        "Star Wars",
-        "Hobbit",
-        "Harry Potter", 
-        "Toy Story"
-    ];
+var topics = [
+    "Home Alone",
+    "Alice in Wonderland",
+    "Lord of the Rings",
+    "Star Wars",
+    "Hobbit",
+    "Harry Potter",
+    "Toy Story"
+];
 
-    // This function will take care of dynamically generating buttons.
-    function generateButtons() {
-        
-        // To avoid having repeated buttons.
-        // Adding new movie buttons.
-        $("#movies-buttons").empty();
-        
-        for (i=0; i < topics.length; i++){
+var searchBoxText = "";                       // ---------------------
+var gifImage = "";
+var gifsAreFull = false;
+var gifRating = "";
 
-            // Buttons for each item in array will be generated. <button>...</button>
-            var a = $("<button>");
-            a.addClass("btn btn-primary");
-            a.addClass("movie");
-            
-            // We apply the attribute data-name='...' to each item in the array.
-            a.attr("data-name", topics[i]);
+// Add user to topics array                 // ---------------------
+function addUserText() {
+    topics.push(searchBoxText);
+}
 
-            a.text(topics[i]);
-            $("#movies-buttons").append(a);
-        }
+// Clear typed text in the search box       // ---------------------
+function clearSearchBox() {
+    searchBoxText = "";
+    $("#search-box").val("");
+}
+
+// This function will take care of dynamically generating buttons.
+function generateButtons() {
+
+    // To avoid having repeated buttons.
+    // Adding new movie buttons.
+    $("#movies-buttons").empty();
+
+    for (i = 0; i < topics.length; i++) {
+        // Buttons for each item in array will be generated. <button>...</button>
+        var a = $("<button>");
+        a.addClass("btn btn-primary");
+        a.addClass("movie");
+
+        // We apply the attribute data-name='...' to each item in the array.
+        a.attr("data-name", topics[i]);
+
+        a.text(topics[i]);
+        $("#movies-buttons").append(a);
     }
+}
 
-    function generateNewButton(){
-        // When search button is pressed ...
-        $("#searchButton").on("click", function(event) {
-            
-            event.preventDefault();
-            
-            var movie = $("#search-box").val().trim();
-            
-            // // In case user doesn't enter anything
-            // if (movie == "") {                            <--- Let's add this functionality later.
-            //     return false;
-            // }
-            
-            topics.push(movie);
-            console.log("Button that was just created: " + movie);
-            console.log(topics);
-            
-            generateButtons();
-            return false;
-        });
-    }
+$(document).ready(function () {
 
-    generateButtons();
-    console.log(topics);
-    generateNewButton(); // If this is missing, entering keyword in <search box> won't generate a new button
-            
+    // generateButtons();
 
-    // User clicks on one of the <keys> on the screen 
-    // Populate 10x GIFS: random, q: random, and 
+    // When search button is pressed ...
+    $("#searchButton").on("click", function (event) {
 
-    // $("<button>").on("click", function(e) {
-    // if (document.getElementsByClassName == "movie" && document.querySelectorAll('[data-name]') == "Harry Postter") {
-    //     console.log("Accio! Accio! Accio!");
-    //     console.log("It didn't work. You are supposed to look at object near you before summoning it!");
-    // }
+        event.preventDefault();
 
+        var movie = $("#search-box").val().trim();
 
-    // function showGifs() {   
-    //           }
+        topics.push(movie);
+        console.log("Button that was just created: " + movie);
+        console.log(topics);
 
+        generateButtons();
+    });
 
+    // Function showing the GIFs
+    function showGifs() {
+        var movie = $(this).attr("data-name");
 
-    function showGifs() { 
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        movie +"&api_key=eMyvfzpHKUO6Vzs4y2ZR5hTOnLM0C84Y&limit=10";
 
-        var gifName = $(this).attr("data-name");
-        console.log(this);
-
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifName + "&api_key=eMyvfzpHKUO6Vzs4y2ZR5hTOnLM0C84Y&limit=10";
-        
-        console.log(queryURL); //    ****** TESTING
         
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then (function(response) {
-            var results = response.data;
-
-            for (j=0; j < results.length; j++) {
-                
-                if (results[j].rating !== "r" && results[j].rating !== "pg-13") {
-                    var gifDiv = $("<div>");
-                    var rating = results[j].rating;
-                    var gifRating = $("<p>").text("Rating: " + rating);
-                    
-                    var movieImage = $("<img>");
-
-                    movieImage.attr("src", results[j].images.fixed_height_still.url); 
-                    // movieImage.attr("data-still", results[j].images.fixed_height_still.url);
-                    // movieImage.addClass("image");
-
-                    gifDiv.append("<p>");
-                    $("#gifsGoHere").prepend(movieImage);
+        }).done(function (response) {   ///or .then
+                clearSearchBox();
+                var results = response.data;
+                if (results == ""){
+                    alert("Give another keyword a try! There is not GIFy for that!");
                 }
-            }
-        });
-    }
+                for (j = 0; j < results.length; j++) { 
+                    var rating = results[j].rating;
+                    var gifDiv = $("<div>");
+                    var gifRating = $("<p><strong>").text("Rating: " + rating);
+                    gifDiv.append(gifRating);
 
-    showGifs();
-});
+                    var movieImage = $("<img>");
+                    movieImage.attr("src", results[j].images.fixed_height_still.url);  // <--- f_h_small_still.url
+                    movieImage.attr("data-still", results[j].images.fixed_height_still.url);
+                    movieImage.attr("data-animate", results[j].images.fixed_height.url);
+                    movieImage.attr("data-state", "still");
+                    movieImage.addClass("image");
+                    
+                    gifDiv.append(movieImage);
+                    $("#gifsGoHere").prepend(gifDiv);
+                } // End of 'for' loop
+
+            });  // End of Document.function(response)
+
+    } // End of Function showGifs()
+
+
+    $(document).on("click", ".movie", showGifs);  // <--- Not working
+    $(document).on("click", ".image", function() {
+        // var that = $(this);
+        var state = $(this).attr('data-state');
+
+        if (state == "still") {
+            $(this).attr("src", $(this).data('animate'));
+            $(this).attr("data-state", "animate");
+        } else if (state == "animate") {
+            $(this).attr("src", $(this).data('still'));
+            $(this).attr("data-state", "still");
+        }
+    });
+
+    generateButtons();  // <----- Need to locally define it! Not working
+
+    // $(document)on("click", ".movie" , function(event) {
+    //     ///           .............
+    // });
+
+});   // End of event listener for movie buttons
